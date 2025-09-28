5fe5cdafa6fa91521447f93215305387210ce0af
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2025-09-28 09:56:14 +0800

    update home route to use new layout view and add layout template

---
+++ pos-management-app/resources/views/layout.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light w-100">
+                <div class="container-fluid w-100">
+                    <a class="navbar-brand" href="#"><h4>Cashier</h4></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="#">Category</a>
+                        <a class="nav-link active" aria-current="page" href="#">Brand</a>
+                        <a class="nav-link active" aria-current="page" href="#">Product</a>
+                        <a class="nav-link active" aria-current="page" href="#">Sales</a>
+                    </div>
+                    </div>
+                </div>
+                </nav>
+        </div>
+    </div>
+
+</body>
+</html>
